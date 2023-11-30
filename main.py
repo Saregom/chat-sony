b44b49cc5f65ab09c75d547cb96eceb9e8281b26
Saregom <santi26reyes@gmail.com>
2023-11-30 08:34:55 -0500

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, url_for, redirect, request, session, jsonify
 
+import statements as st
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret key'
+
+class User():
+    def __init__(self, nombre, correo, numero, cedula, pais):
+        self.nombre = nombre
+        self.correo = correo
+        self.numero = numero
+        self.cedula = cedula
+        self.pais = pais
+
+class var():
+    opcion_correcta = True
+    fin_opciones = False
+    answer_user = []
+    actual_message = ''
+    bloqueo = 1
+    messages = []
 
 @app.route('/') 
 def index():
@@ -11,70 +29,39 @@ def index():
 def login():
     # response = [False, '']
     if request.method == 'POST':
+        rq = request.form
+        var.user = User(rq['nombre'], rq['correo'], rq['numero'], rq['cedula'], rq['pais'])
+
+        var.messages.append({'from':'bot', 
+                             'msg':['Hola '+rq['nombre']+' soy Sonybot, ¿En que te puedo ayudar?']})
         return redirect('/chat')
-    #     response = person_cont.verifyClient(request.form['email'], request.form['password'])
-    #     if response[0]:
-    #         session['user_id'] = response[2]
-    #         session['user_type'] = response[3]
-    #         return redirect('/home')
-
     return render_template('login.html', alert = '')
-
-messages = [{'from':'bot', 'msg':['Hola soy Sonybot, ¿En que te puedo ayudar?']}]
 
 @app.route('/chat')
 def chat():
+    return render_template('chat.html', messages = var.messages)
 
-    return render_template('chat.html', messages = messages)
-
-
-# --------------- 0
-menu_inicial = '''Por favor seleccione una opcion:
-                a) Un producto presenta fallas.
-                b) Información general sobre un producto.
-                c) Preguntas frecuentes. 
-                d) Contactame con un asesor.'''
-
-# --------------- 1
-producto_fallas = '''Lamentamos mucho tu situación porfavor proporciona los siguientes datos en un solo mensaje y en breve un asistente se pondrá en contacto:
-Su correo personal.
-Producto con el cual estas presentando fallas.
-Problemas que presenta el equipo.'''
-info_general = '''Por favor seleccione una opcion:
-                a) SonicVision 4K
-                b) SonicAudio Surround Sound System
-                c) ChillZone Sonic Fridge
-                d) SonicCharge Wireless Charging Station'''
-contacto_asesor = '''Te invitamos al a ingresar al siguiente enlace donde
-                podras contar con la atención de uno de nuestros asistentes:
-                https://www.sony.com.co/corporate/CO/servicioysoporte/servicio.html'''
-
-# ---------------- 2
-info_general_a = '''SonicVision 4k Smart TV:
-                    Experimenta la velocidad en alta definición. Este televisor inteligente de última generación ofrece una calidad de imagen sorprendente y funciones inteligentes para acceder a tus contenidos favoritos'''
-info_general_b = '''SonicAudio Surround Sound System:
-                    Sumérgete en el sonido supersónico con este sistema de sonido envolvente. Desde explosiones hasta música, cada detalle cobra vida para una experiencia auditiva inigualable.'''
-info_general_c = '''ChillZone Sonic Fridge:
-                    Mantén tus bebidas y snacks frescos con estilo. Esta nevera con diseño Sonic no solo es funcional, sino que también agrega un toque de diversión a tu espacio.'''
-info_general_d = '''SonicCharge Wireless Charging Station:
-                    Olvídate de los cables con esta estación de carga inalámbrica. Compatible con una variedad de dispositivos, mantiene tus gadgets cargados mientras muestras tu amor por Sonic.'''
-
-rta_info_falla = '''Gracias por la información proporcionada en breve un asesor te contactara, recomendamos estar atento a su correo.'''
+@app.route('/salir')
+def salir():
+    var.messages.clear()
+    return redirect('/login')
 
 def format(txt):
     return txt.split('\n')
 
-class var():
-    pos_pregunta = 1
-    opcion_correcta = True
-    fin_opciones = False
-    answer_user = []
-    actual_message = ''
-    bloqueo = 1
-
 def incorrect():
     var.actual_message = 'Opcion incorrecta'
     var.opcion_correcta = False
+
+@app.route('/backChat')
+def backChat():
+    if len(var.answer_user) > 0:
+        var.answer_user.pop()
+    
+    if len(var.messages) > 1:
+        var.messages.pop()
+        var.messages.pop()
+    return redirect('/chat')
 
 @app.route('/sendMessage', methods=['POST'])
 def sendMessage():
@@ -87,39 +74,66 @@ def sendMessage():
 
         try:
             if var.answer_user[0] != '':
-                var.actual_message = menu_inicial
+                var.actual_message = st.menu_inicial
 
                 match var.answer_user[1]:
                     case 'a': 
-                        if var.bloqueo == 1:
-                            var.actual_message = producto_fallas
-                            var.bloqueo = 2
-                        else:
-                            if var.answer_user[2] != ' ':
-                                print(444)
-                                var.actual_message = contacto_asesor
-                    case 'b':
-                        var.actual_message = info_general
+                        var.actual_message = st.producto_fallas
                         match var.answer_user[2]:
-                            case 'a': var.actual_message = info_general_a
-                            case 'b': var.actual_message = info_general_b
-                            case 'c': var.actual_message = info_general_c
-                            case 'd': var.actual_message = info_general_d
+                            case 'a': 
+                                var.actual_message = st.falla_tv
+                                match var.answer_user[3]:
+                                    case 'a': var.actual_message = st.tv_control
+                                    case 'b': var.actual_message = st.tv_imagen
+                                    case 'c': var.actual_message = st.tv_audio
+                                    case _: incorrect()
+                            case 'b': 
+                                var.actual_message = st.falla_camara
+                                match var.answer_user[3]:
+                                    case 'a': var.actual_message = st.cam_compatibilidad
+                                    case 'b': var.actual_message = st.cam_webcam
+                                    case 'c': var.actual_message = st.cam_calidadFotos
+                                    case _: incorrect()
+                            case 'c': 
+                                var.actual_message = st.falla_ps
+                                match var.answer_user[3]:
+                                    case 'a': var.actual_message = st.ps_actualizacion
+                                    case 'b': var.actual_message = st.ps_no_enciende
+                                    case 'c': var.actual_message = st.ps_no_reconoce
+                                    case _: incorrect()
+                            case 'd': 
+                                var.actual_message = st.falla_celular
+                                match var.answer_user[3]:
+                                    case 'a': var.actual_message = st.cel_actualizacion
+                                    case 'b': var.actual_message = st.cel_no_enciende
+                                    case 'c': var.actual_message = st.cel_no_carga
+                                    case _: incorrect()
+                            case _: incorrect()
+                    case 'b':
+                        var.actual_message = st.info_general
+                        match var.answer_user[2]:
+                            case 'a': var.actual_message = st.info_general_a
+                            case 'b': var.actual_message = st.info_general_b
+                            case 'c': var.actual_message = st.info_general_c
+                            case 'd': var.actual_message = st.info_general_d
                             case _: incorrect()
                     case 'd':
-                        var.actual_message = contacto_asesor
+                        var.actual_message = st.contacto_asesor
+                        match var.answer_user[2]:
+                                    case 'a': var.actual_message = st.asesor_numero
+                                    case 'b': var.actual_message = st.asesor_correo
+                                    case 'c': var.actual_message = st.asesor_chat
+                                    case _: incorrect()
                     case _: incorrect()
 
         except:
             print('error')
 
-        messages.append({'from':'user', 'msg':format(request.form['message'])})
-        messages.append({'from':'bot', 'msg':format(var.actual_message)})
+        var.messages.append({'from':'user', 'msg':format(request.form['message'])})
+        var.messages.append({'from':'bot', 'msg':format(var.actual_message)})
 
 
-        if var.opcion_correcta:
-            var.pos_pregunta = var.pos_pregunta + 1
-        else:
+        if not var.opcion_correcta:
             var.answer_user.pop()
 
     return redirect('/chat')
---
+++ statements.py
@@ -0,0 +1,96 @@
+# --------------- 0
+menu_inicial = '''Por favor seleccione una opcion:
+                a) Un producto presenta fallas.
+                b) Información general sobre un producto.
+                d) Contactame con un asesor.'''
+
+# --------------- 1
+producto_fallas = '''Indique la categoria de su producto:
+                    a) Tv
+                    b) Camara
+                    c) Play Station
+                    d) Celular'''
+info_general = '''A continuaciion puedes seleccionar uno de nuestros productos mas solicitados:
+                a) SonicVision 4K
+                b) SonicAudio Surround Sound System
+                c) ChillZone Sonic Fridge
+                d) SonicCharge Wireless Charging Station
+
+                De otro modo tambien puedes entrar al siguiente enlace para encontrar mas variedad de productos: 
+                https://www.sony-latin.com/es/electronics/support/manuals'''
+contacto_asesor = '''Selecciona un metodo de contacto para un asesor profesional:
+                    a) Llamada por celular
+                    b) Correo electronico
+                    c) Chat en vivo'''
+
+# ---------------- 2
+falla_tv = '''Indique el area del problema
+            a) Control remoto
+            b) Calidad de imagen
+            c) Audio'''
+falla_camara = '''Indique el area del problema
+            a) Compatibilidad
+            b) Webcam / Streaming
+            c) Calidad de fotos'''
+falla_ps = '''Indique el area del problema
+            a) Actualizacion
+            b) No enciende
+            c) No reconoce videojuego'''
+falla_celular = '''Indique el area del problema
+            a) Actualizacion
+            b) No enciende
+            c) No carga'''
+
+info_general_a = '''SonicVision 4k Smart TV:
+                    Experimenta la velocidad en alta definición. Este televisor inteligente de última generación ofrece una calidad de imagen sorprendente y funciones inteligentes para acceder a tus contenidos favoritos'''
+info_general_b = '''SonicAudio Surround Sound System:
+                    Sumérgete en el sonido supersónico con este sistema de sonido envolvente. Desde explosiones hasta música, cada detalle cobra vida para una experiencia auditiva inigualable.'''
+info_general_c = '''ChillZone Sonic Fridge:
+                    Mantén tus bebidas y snacks frescos con estilo. Esta nevera con diseño Sonic no solo es funcional, sino que también agrega un toque de diversión a tu espacio.'''
+info_general_d = '''SonicCharge Wireless Charging Station:
+                    Olvídate de los cables con esta estación de carga inalámbrica. Compatible con una variedad de dispositivos, mantiene tus gadgets cargados mientras muestras tu amor por Sonic.'''
+
+asesor_numero = '''El numero de contacto para un asesor es el siguiente:
+                Bogota: +57 601 786 8928
+                Nacional: 01-8005-18-97-14'''
+asesor_correo = '''El correo electronico es el siguiente:
+                sonysupportcol@gmail.com'''
+asesor_chat = '''Te invitamos al a ingresar al siguiente enlace donde
+                podras contar con la atención de uno de nuestros asistentes:
+                - Chat en linea: 
+                https://www.service.sony-latin.com/chat-gateway/Home/es
+                - Via Whatsapp:
+                https://api.whatsapp.com/send?phone=528007597669'''
+
+# ------------ 3
+tv_control = '''Visita estos enlaces para seguir los pasos de la posible solucion a tu problema:
+            - Configuracion basica: 
+            https://www.sony-latin.com/es/electronics/support/articles/00030297?category=televisions-projectors&cpint=spt-chatbot
+            
+            - No responde a los comandos: 
+            https://www.sony-latin.com/es/electronics/support/home-video-other-home-video-products/articles/00019429?category=televisions-projectors&cpint=spt-chatbot'''
+tv_imagen = '''Lo mas probable es que presentes lineas negras o imagen borrosa, visita este enlace: 
+https://www.sony-latin.com/es/electronics/support/articles/00032635?category=televisions-projectors&cpint=spt-chatbot'''
+tv_audio = '''Puede que tengas el sonido muy bajo 
+https://www.sony-latin.com/es/electronics/support/articles/00023579?category=televisions-projectors&cpint=spt-chatbot'''
+
+cam_compatibilidad = '''Si necesitas saber las aplicaciones compatibles con tu camara: 
+https://www.sony-latin.com/es/electronics/support/compact-cameras-zv-series/zv-1/articles/00284526'''
+cam_webcam = '''Si deseas saber los pasos a seguir para realizar la conexion a tu pc:
+https://www.sony-latin.com/es/electronics/support/compact-cameras-zv-series/zv-1/articles/00247038'''
+cam_calidadFotos = '''Si presentas fotos descoloridas o imagenes no naturales:
+https://www.sony-latin.com/es/electronics/support/compact-cameras-zv-series/zv-1/articles/00009695'''
+
+ps_actualizacion = '''Para saber los pasos de actualizacion de software:
+https://www.playstation.com/es-co/support/hardware/ps4/system-software/'''
+ps_no_enciende = '''Lo mas probeble es que se deba a problemas del enchufe, o el cable esta dañado, en otro caso, visita este enlace:
+https://es.ifixit.com/Wiki/PlayStation_4_Will_Not_Turn_On#:~:text=Comprueba%20si%20el%20cable%20de,con%20otro%20cable%20de%20alimentaci%C3%B3n.'''
+ps_no_reconoce = '''Procura limpiar bien el disco para quitar huellas o suciedad, puedes visitar el siguiente enlace:
+https://www.playstation.com/es-co/support/hardware/ps4-problems-reading-discs/'''
+
+cel_actualizacion = '''Para actulizar ve a configuracion>sistema>buscar actualizacion, si esto no lo resuelve reinicia o visita el enlace:
+https://helpguide.sony.net/mobile/xperia-1/v1/es/contents/updating-your-device.html'''
+cel_no_enciende = '''Lo mas probable es que este descargado, trata de esperar de 1 a 5 minutos, en otro caso, visita el siguiente enlace:
+https://www.sony.es/electronics/support/articles/00235685'''
+cel_no_carga = '''Verifica que la conexion este bien, si no, intenta con otro cable u otra fuente, de no ser asi, vista el siguiente enlace: 
+https://www.sony.es/electronics/support/articles/00235689'''
